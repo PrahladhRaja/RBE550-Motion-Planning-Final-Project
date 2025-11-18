46826c20222b406175a2b61e670a3bb35ede028d
Quinn Ferguson <quinnferguson48@gmail.com>
2025-11-17 20:12:12 -0500

    add symbolic planner parser

--- code/motion_primitives.py
+++ code/motion_primitives.py
@@ -191,7 +191,46 @@ class MotionPrimitives:
 
 
 
+def parse_symbolic_plan(plan):
+    """
+    Parses a symbolic plan into a sequence of motion primitives.
 
+    Args:
+        plan: A list of symbolic actions.
+    
+    Returns:
+        A list of motion primitives and their arguments.
+    """
+    string2action = {
+        'pick-up': pick_up,
+        'stack': stack,
+        'unstack': unstack
+        }
+    
+    cleaned_plan = plan.replace('(', '').replace(')', '')
+    steps = cleaned_plan.split('\n')
+    print(steps)
+    actions = []
+    for step in steps:
+        step_cleaned = step.split()
+        primative = string2action[step_cleaned[0]]
+        args = [color[0] for color in step_cleaned[2:]]
+        actions.append((primative, args))
+    return actions
+
+if __name__ == "__main__":
+    test_plan = """(pick-up r magenta)
+    (stack r magenta cyan)
+    (pick-up r yellow)
+    (stack r yellow magenta)
+    (pick-up r green)
+    (stack r green blue)
+    (pick-up r red)
+    (stack r red green)"""
+    
+    actions = parse_symbolic_plan(test_plan)
+    for action in actions:
+        print(action)
 
 
 
